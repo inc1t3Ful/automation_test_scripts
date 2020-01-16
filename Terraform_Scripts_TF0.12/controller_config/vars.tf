b9757f72f5cb44ecd9ff807559a6316b889ab833
Anthony <ant.lee813@gmail.com>
2020-01-15 18:08:27 -0800

    Remove controller_config cred var
    ** now source env

--- Terraform_Scripts_TF0.12/controller_config/vars.tf
+++ Terraform_Scripts_TF0.12/controller_config/vars.tf
@@ -1,7 +1,3 @@
-variable "aviatrix_controller_ip" {}
-variable "aviatrix_controller_username" {}
-variable "aviatrix_controller_password" {}
-
 variable "toggle_http_access" {}
 variable "toggle_fqdn_exception" {}
 variable "sg_account_name" {}
